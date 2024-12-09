cf2c57a253bf6f8de7669ac248a38ff789f96379
Fadi Abji <fadi@wexer.se>
2024-12-09 13:36:35 +0100

    Add project files.

---
+++ UmbracoProject2/Program.cs
@@ -0,0 +1,36 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+//Blazor integration
+builder.Services.AddServerSideBlazor();
+
+builder.CreateUmbracoBuilder()
+    .AddBackOffice()
+    .AddWebsite()
+    .AddDeliveryApi()
+    .AddComposers()
+    .Build();
+
+WebApplication app = builder.Build();
+
+await app.BootUmbracoAsync();
+
+//Blazor integration
+app.MapBlazorHub();
+
+
+app.UseHttpsRedirection();
+
+app.UseUmbraco()
+    .WithMiddleware(u =>
+    {
+        u.UseBackOffice();
+        u.UseWebsite();
+    })
+    .WithEndpoints(u =>
+    {
+        u.UseInstallerEndpoints();
+        u.UseBackOfficeEndpoints();
+        u.UseWebsiteEndpoints();
+    });
+
+await app.RunAsync();
